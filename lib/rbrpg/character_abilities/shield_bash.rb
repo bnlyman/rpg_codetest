6169c103b95c3fb80f014e507c19c000f7959662
ben lyman <bnlyman@gmail.com>
2015-05-26 16:40:52 -0600

    Created shield_bash.rb

---
+++ lib/rbrpg/character_abilities/shield_bash.rb
@@ -0,0 +1,19 @@
+require 'rbrpg/decorated'
+require 'rbrpg/character_abilities/traits/deals_damage'
+
+module Rbrpg
+  module CharacterAbilities
+    class ShieldBash < Ability
+      include ::Rbrpg::Decorated
+      include ::Rbrpg::CharacterAbilities::Traits::DealsDamage
+
+      def initialize(*args)
+        super(*args)
+      end
+
+      def damage_source
+        @source.weapon
+      end
+    end
+  end
+end
